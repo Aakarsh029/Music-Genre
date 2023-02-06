8886b1b1d42e7530e853a251393b4d9cd6ceb4a0
Aakarsh029 <96654493+Aakarsh029@users.noreply.github.com>
2023-02-06 22:29:32 +0530

    front end + backend in collab

---
+++ predictor/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+from django.conf import settings
+import os
+import pickle
+
+class PredictorConfig(AppConfig):
+    # create path to models
+    # change
+    name = 'predictor'
